1e14003117115894cdd339edd8b1029682d5ecf3
Sam Casey <samcaseydesign@gmail.com>
2015-08-19 01:05:46 -0500

    changes to .js files

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var points = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function() {
 
     var pointsArray = function() {
         var newArray = [];
@@ -9,11 +9,6 @@ var animatePoints = function() {
         }
         return newArray;
     };
-
-    function forEach (array, callback) {
-        array();
-        callback();
-    }
 
     function callbackFunc() {
         points[i].style.opacity = 1;
@@ -44,6 +39,56 @@ window.onload = function() {
     });
 
 };
+
+///////////////////////////////////---------------------------------------------------------––––––––––––––––––––––
+
+//var pointsArray = document.getElementsByClassName('point');
+//
+//var animatePoints = function(points) {
+//
+//    var revealFirstPoint = function() {
+//        points[0].style.opacity = 1;
+//        points[0].style.transform = "scaleX(1) translateY(0)";
+//        points[0].style.msTransform = "scaleX(1) translateY(0)";
+//        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+//    };
+//
+//    var revealSecondPoint = function() {
+//        points[1].style.opacity = 1;
+//        points[1].style.transform = "scaleX(1) translateY(0)";
+//        points[1].style.msTransform = "scaleX(1) translateY(0)";
+//        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
+//    };
+//
+//    var revealThirdPoint = function() {
+//        points[2].style.opacity = 1;
+//        points[2].style.transform = "scaleX(1) translateY(0)";
+//        points[2].style.msTransform = "scaleX(1) translateY(0)";
+//        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
+//    };
+//
+//    revealFirstPoint();
+//    revealSecondPoint();
+//    revealThirdPoint();
+//
+//};
+//
+//window.onload = function() {
+//
+//    if (window.innerHeight > 1200) {
+//        animatePoints(pointsArray);
+//    }
+//
+//    window.addEventListener('scroll', function() {
+//
+//        //console.log("Current offset from the top is " + pointsArray[0].getBoundingClientRect().top + " pixels");
+//        if (pointsArray[0].getBoundingClientRect().top <= 700) {
+//            animatePoints(pointsArray);
+//        }
+//
+//    });
+//
+//};
 
 
 
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -1,33 +1,52 @@
 var points = document.getElementsByClassName('point');
 
-var pointsArray = function() {
-    var newArray = [];
-    for (var i = 0; i < points.length; i++) {
-        newArray.push(points[i])
+var animatePoints = function(points) {
+
+    var pointsArray = function () {
+        var newArray = [];
+        for (var i = 0; i < points.length; i++) {
+            newArray.push(points[i])
+        }
+        return newArray;
+    };
+
+    function myCallback(index, element) {
+        console.log("The element at index " + index + " is " + element);
     }
-    return newArray;
+
+    function forEach(array, callback) {
+        // 1. Iterate over the array
+        // 1a. Execute a callback for every item
+        // 1b. Pass in the element, and its index into the callback execution
+        // Array.prototype.forEach(index, element, array);
+
+        // iterate over the array
+        for (var i = 0; i < array.length; i++) {
+            // execute the call back for every element
+            callback(i, array[i], array);
+            // callback(index, element, array)
+        }
+    }
+
+    forEach(pointsArray(), myCallback);
+
 };
-//
-//pointsArray().forEach(function(element) {
-//    console.log(element + " has been logged.");
-//});
 
-function myCallback(index, element) {
-    console.log("The element at index " + index + " is " + element);
-}
+animatePoints();
 
-function forEach(array, callback) {
-    // 1. Iterate over the array
-    // 1a. Execute a callback for every item
-    // 1b. Pass in the element, and its index into the callback execution
-    // Array.prototype.forEach(index, element, array);
+window.onload = function() {
 
-    // iterate over the array
-    for (var i = 0; i < array.length; i++) {
-        // execute the call back for every element
-        callback(i, array[i], array);
-        // callback(index, element, array)
+    if (window.innerHeight > 1200) {
+        animatePoints(pointsArray);
     }
-}
 
-forEach(pointsArray(), myCallback);
+    window.addEventListener('scroll', function() {
+
+        //console.log("Current offset from the top is " + pointsArray[0].getBoundingClientRect().top + " pixels");
+        if (pointsArray[0].getBoundingClientRect().top <= 700) {
+            animatePoints(pointsArray);
+        }
+
+    });
+
+};

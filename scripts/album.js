372d2d0930d8f0f8384ec6c6fc82d4cdb69abe63
Sam Casey <samcaseydesign@gmail.com>
2015-08-19 23:14:21 -0500

    assignment 27 - fixes to findParentByClassName function

--- scripts/album.js
+++ scripts/album.js
@@ -72,11 +72,15 @@ var setCurrentAlbum = function(album) {
 var findParentByClassName = function(element, targetClass) {
 
     var currentParent = element.parentElement;
+
     if (currentParent === null) {
-        alert("No Parent Found.")
-    } else {
-        while (currentParent.className != targetClass) {
-            currentParent = currentParent.parentElement
+        alert("No Parent Found.");
+    }
+    while (currentParent.className != targetClass) {
+        currentParent = currentParent.parentElement
+        if (currentParent === null) {
+            alert("No Parent Found.");
+            break;
         }
     }
 
@@ -84,23 +88,38 @@ var findParentByClassName = function(element, targetClass) {
 
 };
 
+//var findParentByClassName = function(element, targetClass) {
+//
+//    var currentParent = element.parentElement;
+//    if (currentParent === null) {
+//        alert("No Parent Found.")
+//    } else {
+//        while (currentParent.className != targetClass) {
+//            currentParent = currentParent.parentElement
+//        }
+//    }
+//
+//    return currentParent;
+//
+//};
+
 var getSongItem = function(element) {
 
-switch (element.className) {
-    case 'album-song-button':
-    case 'ion-play':
-    case 'ion-pause':
-        return findParentByClassName(element, 'song-item-number');
-    case 'album-view-song-item':
-        return element.querySelector('.song-item-number');
-    case 'song-item-title':
-    case 'song-item-duration':
-        return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-    case 'song-item-number':
-        return element;
-    default:
-        return;
-    }
+    switch (element.className) {
+        case 'album-song-button':
+        case 'ion-play':
+        case 'ion-pause':
+            return findParentByClassName(element, 'song-item-number');
+        case 'album-view-song-item':
+            return element.querySelector('.song-item-number');
+        case 'song-item-title':
+        case 'song-item-duration':
+            return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+        case 'song-item-number':
+            return element;
+        default:
+            return;
+        }
 };
 
 var clickHandler = function(targetElement) {
@@ -161,4 +180,5 @@ window.onload = function() {
             clickHandler(event.target);
         });
     }
+
 };

6c32bfa5a441bdb5218319d953158450b9a5e3b4
Sam Casey <samcaseydesign@gmail.com>
2015-08-20 23:25:18 -0500

    Added jQuery to landing.js

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,51 +1,33 @@
 
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
+var animatePoints = function() {
 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function() {
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
     };
-
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-
+    $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
+$(window).load (function() {
 
-    if (window.innerHeight > 1200) {
-        animatePoints(pointsArray);
+    if ($(window).height() > 1200) {
+        animatePoints();
     }
 
-    window.addEventListener('scroll', function() {
+    $(window).scroll(function(event) {
 
         //console.log("Current offset from the top is " + pointsArray[0].getBoundingClientRect().top + " pixels");
-        if (pointsArray[0].getBoundingClientRect().top <= 700) {
-            animatePoints(pointsArray);
+        if ($(window).scrollTop() >= 700) {
+            animatePoints();
         }
 
     });
 
-};
+});
 
 
 

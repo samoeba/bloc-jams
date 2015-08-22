49b52f5b86e0981274ca0f3c43acc1326fc12a96
Sam Casey <samcaseydesign@gmail.com>
2015-08-22 02:23:55 -0500

    Refactored collection and album pages with jQuery

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,6 @@
-var collectionItemTemplate =
+var buildCollectionItemTemplate = function() {
+
+    var template =
     '<div class="collection-album-container column fourth">'
     + '  <img src="assets/images/album_covers/01.png"/>'
     + '  <div class="collection-album-info caption">'
@@ -13,16 +15,24 @@ var collectionItemTemplate =
     + '  </div>'
     + '</div>';
 
-window.onload = function() {
-
-    // #1
-    var collectionContainer = document.getElementsByClassName('album-covers')[0].childNodes[1];
-    // #2
-    collectionContainer.innerHTML = '';
-
-    // #3
-    for (var i = 0; i < 12; i++) {
-        collectionContainer.innerHTML += collectionItemTemplate;
-    }
+    return $(template);
 
 };
+
+$(window).load(function() {
+
+    // #1
+    var $collectionContainer = $('.album-covers .clearfix');
+
+    // #2
+    $collectionContainer.empty();
+
+    // #3
+
+
+    for (var i = 0; i < 12; i++) {
+        var $newThumbnail = buildCollectionItemTemplate();
+
+        $collectionContainer.append($newThumbnail);
+    }
+});
